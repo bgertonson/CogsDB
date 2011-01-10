45974b223d11d3e96dcdf1e7a47d8aaa2a6ff377
Bryan Gertonson <bryan.gertonson@gmail.com>
2011-01-10 07:49:46 -0600

    Changes to unit tests to run against clean sql server ce database on each run (copy on build) might need slightly modified strategy to support multiple runs between builds.
    Changes to IdServer and identity generation to allow it to work properly with sql server ce (can run multiple statements or define variables)

---
+++ src/CogsDB.Engine/CogsSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace CogsDB.Engine
+{
+    internal static class CogsSystem
+    {
+        public static class Ids
+        {
+            public const string IdentityTrackingDocument = ".sys.docs.id-tracking";
+        }
+    }
+}
---
+++ src/CogsDB.Engine/Documents/IdTrackingDocument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CogsDB.Engine.Documents
+{
+    public class IdTrackingDocument
+    {
+        public IdTrackingDocument()
+        {
+            Trackers = new List<IdTracker>();
+            Id = CogsSystem.Ids.IdentityTrackingDocument;
+        }
+
+        public String Id { get; set; }
+
+        public List<IdTracker> Trackers { get; set; }
+
+        public IdTracker GetTracker<T>()
+        {
+            var type = typeof(T);
+            return GetTracker(type);
+        }
+
+        public IdTracker GetTracker(Type type)
+        {
+            var name = type.Name;
+            var tracker = Trackers.FirstOrDefault(x => x.Type == name);
+            if (tracker == null)
+            {
+                tracker = new IdTracker { Type = name, LastBlock = 1, BlockSize = 5 };
+                Trackers.Add(tracker);
+            }
+            return tracker;
+        }
+
+        public class IdTracker
+        {
+            public string Type { get; set; }
+            public int LastBlock { get; set; }
+            public int BlockSize { get; set; }
+
+            public void Increment()
+            {
+                LastBlock++;
+            }
+        }
+    }
+}
