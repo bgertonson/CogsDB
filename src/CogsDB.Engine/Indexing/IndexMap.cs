77dd8df6cb3196bc630bd5af1795ff3b04f9f853
Bryan Gertonson <bryan.gertonson@gmail.com>
2011-03-18 11:28:17 -0500

    Adding lucene libraries for indexing to be implemented later Testing for thread safety

---
+++ src/CogsDB.Engine/Indexing/IndexMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CogsDB.Engine.Indexing
+{
+    public class IndexMap
+    {
+        private IList<IndexMapEntry> _entries = new List<IndexMapEntry>();
+        
+        public void AddEntry(IndexMapEntry entry)
+        {
+            _entries.Add(entry);
+        }
+
+        public static IndexMap InferMap<T>()
+        {
+            var map = new IndexMap();
+            Type type = typeof (T);
+            var properties = type.GetProperties();
+            foreach (var propertyInfo in properties)
+            {
+                if (!propertyInfo.PropertyType.Equals(typeof(String))) continue;
+
+                IndexMapEntry entry;
+                var name = propertyInfo.Name;
+                if(name.Equals("id", StringComparison.OrdinalIgnoreCase))
+                {
+                    entry = new IndexMapEntry(name, true, false);
+                    map.AddEntry(entry);
+                    continue;
+                }
+
+                entry = new IndexMapEntry(name, true, false);
+                map.AddEntry(entry);
+            }
+            return map;
+        }
+    }
+
+    public class IndexMapEntry
+    {
+        public IndexMapEntry(string field, bool store, bool analyze)
+        {
+            Field = field;
+            Store = store;
+            Analyze = analyze;
+        }
+
+        public string Field { get; private set; }
+        public bool Store { get; private set; }
+        public bool Analyze { get; private set; }
+    }
+}
---
+++ src/Tests/Integration/ThreadTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using CogsDB.Engine;
+using NUnit.Framework;
+using Tests.Data;
+
+namespace Tests.Integration
+{
+    [TestFixture]
+    public class ThreadTests
+    {
+        private ManualResetEvent _reset = new ManualResetEvent(false);
+        private int _count = 0;
+        private int _expected = 0;
+        private object _lock = new object();
+
+        [Test]
+        public void Add100People_UsingThreadPool()
+        {
+            var rnd = new Random();
+            var first = new[]
+                            {
+                                "Bob", "Frank", "Bill", "Martha", "Joan", "Jacob", "Franz", "Gunther", "Gabrielle",
+                                "Ashley"
+                            };
+            var last = new[] { "Twilliger", "Jones", "Smith", "Baker", "Ferdinand", "Cook", "Thompson" };
+            var domain = new[] { "gmail", "yahoo", "hotmail" };
+
+
+            _expected = 100;
+            for (int i = 0; i < _expected; i++ )
+            {
+                var f = first[rnd.Next(first.Length)];
+                var l = last[rnd.Next(last.Length)];
+                var d = domain[rnd.Next(domain.Length)];
+
+                var person = new Person()
+                {
+                    FirstName = f,
+                    LastName = l,
+                    Email = String.Format("{0}.{1}@{2}.com", f, l, d)
+                };
+
+                ThreadPool.QueueUserWorkItem(ThreadPoolWork, person);
+            }
+            _reset.WaitOne();
+        }
+
+        private void ThreadPoolWork(object data)
+        {
+            var person = data as Person;
+
+            var session = GetCogsSession();
+            session.Store(person);
+            session.SubmitChanges();
+
+            lock (_lock)
+            {
+                _count++;
+                if (_count == _expected)
+                    _reset.Set();
+            }
+
+        }
+
+        private CogsSession GetCogsSession()
+        {
+            var persister = new SqlPersister("sqlce");
+            var serializer = new JsonSerializer();
+            return new CogsSession(persister, serializer);
+        }
+
+    }
+}
